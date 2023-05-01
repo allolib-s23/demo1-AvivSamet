d721b34c43aa2de57490fc488f08d1075b589938
AvivSamet <aviv@ucsb.edu>
2023-05-01 13:36:56 -0700

    commited progress from stuff that i forgot to push earlier

---
+++ tutorials/synthesis/Slap_Bass_Demo.cpp
@@ -0,0 +1,17 @@
+#include "Gamma/Analysis.h"
+#include "Gamma/Effects.h"
+#include "Gamma/Envelope.h"
+#include "Gamma/Oscillator.h"
+
+#include "al/app/al_App.hpp"
+#include "al/scene/al_PolySynth.hpp"
+#include "al/scene/al_SynthSequencer.hpp"
+#include "al/ui/al_ControlGUI.hpp"
+#include "al/ui/al_Parameter.hpp"
+
+namespace al{
+    class SlapBass : public PolySynth {
+    
+    };
+}
+
